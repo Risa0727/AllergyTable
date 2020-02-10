1e35da3e267baece7d448159e04290cc86a7a126
RisaU <from_aquoa@yahoo.co.jp>
2020-02-10 23:06:25 +1100

    ラーメン選択をリストからプルダウンに変更

--- function.js
+++ function.js
@@ -3,17 +3,14 @@ $(function() {
    * https://codingmania.net/table/109.html
    * テーブルのデータ・行を抽出・表示
    */
-$("ul.mod_koumokulist li a").click(function() {
-  var ramen = $(this).attr("class");
-  $("tbody tr").css("display", "none");
-  $("tbody tr.title,tr#" + ramen).show();
-
-  return false;
-});
-  // display all
-  $("li.mod_LabelAll a").click(function() {
-    $("tbody tr").show();
-
-    return false;
+  $('[name="menu-category"]').change(function() {
+    var category = $('[name="menu-category"]').val();
+    console.log(category);
+    if (category == 'all') {
+      $("tbody tr").show();
+    } else {
+      $("tbody tr").css("display", "none");
+      $("tbody tr." + category).show();
+    }
   });
 });
