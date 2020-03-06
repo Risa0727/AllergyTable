43fabd88aab1e3b88d128c609f79b567ee7ad557
RisaU <from_aquoa@yahoo.co.jp>
2020-03-07 00:27:35 +1100

    検索ワードを含むラーメンを非表示にする機能を追加 参考: https://www.tam-tam.co.jp/tipsnote/javascript/post11315.html

---
+++ searchWord.js
@@ -0,0 +1,52 @@
+/**
+ * 検索ワードを含むラーメンを非表示にする機能
+ * 課題：
+ *  複数の条件で検索できるようにしたい
+ *  予測補完機能追加したい
+ */
+$(function() {
+  searchWord = function() {
+    // 変数宣言
+    var searchResult,
+    searchText = $(this).val(), 
+    targetText,
+    targetClass;
+
+    // 配列宣言
+    searchResult = [];
+
+    // 検索ボックスに値が入ってる時、
+    if (searchText != '') {
+      $('thead th').each(function() {
+        targetText = $(this).text();
+        
+
+        // 検索ワードが一致したら,
+        if (targetText.indexOf(searchText) != -1) {
+          // クラス名取得
+          targetClass = $(this).attr("class");
+          console.log('hoge: ' + targetClass);
+        }
+      });
+
+      $('tbody tr').each(function() {
+        $('tbody td').each(function() {
+         if ($(this).attr("class") == targetClass) {
+           // ターゲットのカラムに”o”が無いか調べる
+           if ($(this).text() != "o") {
+            $(this).parent().removeClass('hidden');
+           } else {
+            $(this).parent().addClass('hidden');
+           }
+         }
+        });
+      });
+    } else {
+      // カラになったら全て表示
+      $("tbody tr").removeClass('hidden');
+    }
+  };
+
+  // execute
+  $('#search-text').on('input', searchWord);
+});
