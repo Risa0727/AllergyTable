30d0ee42022d835e3ea5798d371f19db2953ee1c
RisaU <from_aquoa@yahoo.co.jp>
2020-03-08 14:24:34 +1100

    アレルギー検索をテキスト入力からプルダウン選択に変更 （タブレットで使用する場合テキスト入力だと使いづらい）

--- function.js
+++ function.js
@@ -5,12 +5,51 @@ $(function() {
    */
   $('[name="menu-category"]').change(function() {
     var category = $('[name="menu-category"]').val();
-    console.log(category);
+    // console.log(category);
     if (category == 'all') {
       $("tbody tr").show();
     } else {
       $("tbody tr").css("display", "none");
       $("tbody tr." + category).show();
+    }
+  });
+
+  /**
+   * アレルギー物質検索
+   */
+  $('[name="allergy-category"]').change(function() {
+    var searchCategory = $('[name="allergy-category"]').val();
+    var targetText,
+        targetClass;
+
+    console.log(searchCategory);
+    if (searchCategory == 'noAllergy') {
+      // $("tbody tr").show();
+      $("tbody tr").removeClass('hidden');
+    } else {
+      // テーブルヘッダーからターゲットカラムの位置を特定
+      $('thead th').each(function() {
+        targetText = $(this).text();
+        
+        if (targetText.indexOf(searchCategory) != -1) {
+          targetClass = $(this).attr("class");
+          // console.log('カラム: ' + targetClass);
+        }
+      });
+      // テーブル一覧から該当する行を非表示にする
+      $('tbody tr').each(function() {
+        $('tbody td').each(function() {
+         if ($(this).attr("class") == targetClass) {
+           // ターゲットのカラムに”o”が無いか調べる
+           if ($(this).text() != "o") {
+            $(this).parent().removeClass('hidden');
+           } else {
+            $(this).parent().addClass('hidden');
+           }
+         }
+        });
+      });
+      
     }
   });
 });
